b3e9aa5e79756bc2a6c81aa188c4d2f5f81e8159
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-30 16:32:50 +0300

    Fixed warning.

--- src/main/java/g0001_0100/s0034_find_first_and_last_position_of_element_in_sorted_array/Solution.java
+++ src/main/java/g0001_0100/s0034_find_first_and_last_position_of_element_in_sorted_array/Solution.java
@@ -4,11 +4,8 @@ package g0001_0100.s0034_find_first_and_last_position_of_element_in_sorted_array
 // #Algorithm_II_Day_1_Binary_Search #Binary_Search_I_Day_5
 // #2022_02_18_Time_1_ms_(30.87%)_Space_47.1_MB_(20.91%)
 
-@SuppressWarnings("unused")
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
         int[] ans = new int[2];
         ans[0] = helper(nums, target, false);
         ans[1] = helper(nums, target, true);
